a544fcc2af4a29779afad8d73205dbb58a70d647
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2024-02-28 17:29:11 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,96 @@
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class BluetoothService {
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // UUID for SPP (Serial Port Profile)
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDevice bluetoothDevice;
+    private BluetoothSocket bluetoothSocket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    private Handler handler;
+
+    public BluetoothService(Handler handler) {
+        this.handler = handler;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void connectToDevice(String address) {
+        bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+        try {
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(MY_UUID);
+            bluetoothSocket.connect();
+            inputStream = bluetoothSocket.getInputStream();
+            outputStream = bluetoothSocket.getOutputStream();
+            ConnectedThread connectedThread = new ConnectedThread();
+            connectedThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connectionFailed();
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private byte[] buffer;
+
+        public ConnectedThread() {
+            buffer = new byte[1024];
+        }
+
+        public void run() {
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = inputStream.read(buffer);
+                    handler.obtainMessage(MainActivity.MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    connectionLost();
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] bytes) {
+            try {
+                outputStream.write(bytes);
+                handler.obtainMessage(MainActivity.MESSAGE_WRITE, -1, -1, bytes).sendToTarget();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void write(byte[] bytes) {
+        ConnectedThread connectedThread = new ConnectedThread();
+        connectedThread.write(bytes);
+    }
+
+    private void connectionFailed() {
+        Message message = handler.obtainMessage(MainActivity.MESSAGE_TOAST);
+        message.obj = "Unable to connect device";
+        handler.sendMessage(message);
+    }
+
+    private void connectionLost() {
+        Message message = handler.obtainMessage(MainActivity.MESSAGE_TOAST);
+        message.obj = "Device connection was lost";
+        handler.sendMessage(message);
+    }
+
+    public void cancel() {
+        try {
+            bluetoothSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
